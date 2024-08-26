0d400b3656e59480d2bdb6dfe61437078ae7ebed
mudo <dgny362@gmail.com>
2024-08-26 18:47:54 +0300

    navbar oluşturuldu

--- lib/main.dart
+++ lib/main.dart
@@ -9,6 +9,51 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.white,
+        body: SafeArea(
+            child: Center(
+          child: Column(),
+        )),
+        bottomNavigationBar: NavigationBar(
+          indicatorColor: Colors.white,
+          backgroundColor: Colors.white,
+          destinations: const <Widget>[
+            NavigationDestination(
+              selectedIcon: Icon(
+                Icons.home,
+                color: Colors.orange,
+              ),
+              icon: Icon(Icons.home_outlined),
+              label: 'Anasayfa',
+            ),
+            NavigationDestination(
+                selectedIcon: Icon(
+                  Icons.favorite_outlined,
+                  color: Colors.orange,
+                ),
+                icon: Icon((Icons.favorite_border)),
+                label: 'Favoriler'),
+            NavigationDestination(
+              selectedIcon: Icon(
+                Icons.shopping_cart,
+                color: Colors.orange,
+              ),
+              icon: Icon(Icons.shopping_cart_outlined),
+              label: 'Sepet',
+            ),
+            NavigationDestination(
+              selectedIcon: Icon(
+                Icons.person,
+                color: Colors.orange,
+              ),
+              icon: Icon(Icons.person_outline_outlined),
+              label: 'Saved',
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
