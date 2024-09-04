4fe8a440e7a534f4061740f46084378575d6013f
mudo <dgny362@gmail.com>
2024-09-04 15:34:50 +0300

    veritabanı baglantısı yapıldı

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,13 @@
 import 'package:flutter/material.dart';
-
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_storage/firebase_storage.dart';
 import 'Anasayfa.dart';
 import 'Favoriler.dart';
 import 'Profil.dart';
 import 'Sepet.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
   runApp(const MyApp());
 }
 
@@ -19,56 +21,80 @@ class MyApp extends StatefulWidget {
 class _MyAppState extends State<MyApp> {
   int indexs = 0;
   final ekranlar = [Anasayfa(), Favoriler(), Sepet(), Profil()];
+  final Future<FirebaseApp> _initilaztion = Firebase.initializeApp();
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        backgroundColor: Colors.white,
-        body: ekranlar[indexs],
-        bottomNavigationBar: NavigationBar(
-          indicatorColor: Colors.white,
-          backgroundColor: Colors.white,
-          selectedIndex: indexs,
-          onDestinationSelected: (int newIndex) {
-            setState(() {
-              indexs = newIndex;
-            });
-          },
-          destinations: const <Widget>[
-            NavigationDestination(
-              selectedIcon: Icon(
-                Icons.home,
-                color: Colors.orange,
-              ),
-              icon: Icon(Icons.home_outlined),
-              label: 'Anasayfa',
-            ),
-            NavigationDestination(
-                selectedIcon: Icon(
-                  Icons.favorite_outlined,
-                  color: Colors.orange,
-                ),
-                icon: Icon((Icons.favorite_border)),
-                label: 'Favoriler'),
-            NavigationDestination(
-              selectedIcon: Icon(
-                Icons.shopping_cart,
-                color: Colors.orange,
-              ),
-              icon: Icon(Icons.shopping_cart_outlined),
-              label: 'Sepet',
-            ),
-            NavigationDestination(
-              selectedIcon: Icon(
-                Icons.person,
-                color: Colors.orange,
-              ),
-              icon: Icon(Icons.person_outline_outlined),
-              label: 'Profil',
-            ),
-          ],
-        ),
-      ),
-    );
+        home: FutureBuilder(
+            future: _initilaztion,
+            builder: (context, snapshot) {
+              if (snapshot.hasError) {
+                return Center(
+                  child: Text('Beklenmeyen bir hata oluştu'),
+                );
+              } else if (snapshot.hasData) {
+                return Scaffold(
+                  backgroundColor: Colors.white,
+                  body: ekranlar[indexs],
+                  bottomNavigationBar: NavigationBar(
+                    indicatorColor: Colors.white,
+                    backgroundColor: Colors.white,
+                    selectedIndex: indexs,
+                    onDestinationSelected: (int newIndex) {
+                      setState(() {
+                        indexs = newIndex;
+                      });
+                    },
+                    destinations: const <Widget>[
+                      NavigationDestination(
+                        selectedIcon: Icon(
+                          Icons.home,
+                          color: Colors.orange,
+                        ),
+                        icon: Icon(Icons.home_outlined),
+                        label: 'Anasayfa',
+                      ),
+                      NavigationDestination(
+                          selectedIcon: Icon(
+                            Icons.favorite_outlined,
+                            color: Colors.orange,
+                          ),
+                          icon: Icon((Icons.favorite_border)),
+                          label: 'Favoriler'),
+                      NavigationDestination(
+                        selectedIcon: Icon(
+                          Icons.shopping_cart,
+                          color: Colors.orange,
+                        ),
+                        icon: Icon(Icons.shopping_cart_outlined),
+                        label: 'Sepet',
+                      ),
+                      NavigationDestination(
+                        selectedIcon: Icon(
+                          Icons.person,
+                          color: Colors.orange,
+                        ),
+                        icon: Icon(Icons.person_outline_outlined),
+                        label: 'Profil',
+                      ),
+                    ],
+                  ),
+                );
+              } else {
+                return Center(
+                  child: CircularProgressIndicator(),
+                );
+              }
+            }));
   }
 }
+/**/
+/*FutureBuilder(
+future: _initilaztion,
+builder: (context, snapshot) {
+if (snapshot.hasError) {
+return Center(
+child: Text('Beklenmeyen bir hata oluştu'),
+);
+}
+}),*/
